77f3ad82a7cc4924a21ee469f9768ad1c2e73676
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-04-09 12:34:46 +0200

    set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/template-3/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
